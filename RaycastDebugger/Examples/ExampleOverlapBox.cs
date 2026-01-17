c52ac737fb06e6f0291f3547b9e75edc7a0eb17e
BOLOBEEF <stephanoashraf64@gmail.com>
2026-01-17 11:31:31 +0200

    Update ExampleOverlapBox.cs

--- RaycastDebugger/Examples/ExampleOverlapBox.cs
+++ RaycastDebugger/Examples/ExampleOverlapBox.cs
@@ -1,16 +1,18 @@
+using System.Linq;
 using UnityEngine;
 
 public class ExampleOverlapBox : MonoBehaviour
 {
+    [SerializeField] private float size = 2f;
 
     void FixedUpdate()
     {
         Vector3 position = transform.position;
-        Vector3 halfExtents = Vector3.one;
+        Vector3 halfExtents = Vector3.one * size;
         Quaternion orientation = transform.rotation;
 
         Collider[] colliders = Physics.OverlapBox(position, halfExtents, orientation);
 
-        RaycastDebugger.DebugOverlapBox(position, halfExtents, orientation, colliders, Color.red);
+        RaycastDebugger.DebugOverlapBox(position, halfExtents, orientation, colliders, colliders.Count() == 0 ? Color.white : Color.red);
     }
 }
