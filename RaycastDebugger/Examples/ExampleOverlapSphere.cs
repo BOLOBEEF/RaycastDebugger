e3238d2d90a0c105e63119c90a4565918a802fa1
BOLOBEEF <stephanoashraf64@gmail.com>
2026-01-17 11:32:20 +0200

    Update ExampleOverlapSphere.cs

--- RaycastDebugger/Examples/ExampleOverlapSphere.cs
+++ RaycastDebugger/Examples/ExampleOverlapSphere.cs
@@ -1,15 +1,16 @@
+using System.Linq;
 using UnityEngine;
 
 public class ExampleOverlapSphere : MonoBehaviour
 {
+    [SerializeField] private float radius = 2f;
 
     void FixedUpdate()
     {
         Vector3 position = transform.position;
-        float radius = 1f;
 
         Collider[] colliders = Physics.OverlapSphere(position, radius);
 
-        RaycastDebugger.DebugOverlapSphere(position, radius, colliders, Color.red);
+        RaycastDebugger.DebugOverlapSphere(position, radius, colliders, colliders.Count() == 0 ? Color.white : Color.red);
     }
 }
